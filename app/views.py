8eeec6931255dd007cfedd6fae0190f936a0f2e8
Luke <akabaker@gmail.com>
2012-05-29 01:26:09 -0500

    updated

--- app/views.py
+++ app/views.py
@@ -2,6 +2,17 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseForbidden, Http404
 from django.template import RequestContext
+from app.forms import UploadFile
 
 def index(request):
-	return render_to_response('base.html', context_instance = RequestContext(request))
+	return render_to_response('home.html', context_instance = RequestContext(request))
+
+def upload_file(request):
+	if request.method == 'POST':
+		form = UploadFile(request.POST, request.FILES)
+		if form.is_valid():
+			handle_uploaded_file(request.FILES['file'])
+			return HttpResponse('success')
+	else:
+		form = UploadFileForm()
+	return render_to_response('upload.html', {'form': form})	
